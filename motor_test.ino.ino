7848c6c2231615b1ae96144a34dc3c854282a193
RolfKro <rkraye16@student.aau.dk>
2017-11-10 11:31:47 +0100

    Add files via upload

---
+++ motor_test.ino.ino
@@ -0,0 +1,30 @@
+const int servo=9;
+const int dc_has=10;
+const int dc_ret1=12;
+const int dc_ret2=13;
+
+unsigned long tid;
+
+void setup() 
+{ 
+  pinMode(servo, OUTPUT);   // sets the pin as output
+  pinMode(dc_has, OUTPUT);   // sets the pin as output
+  pinMode(dc_ret1, OUTPUT);   // sets the pin as output
+  pinMode(dc_ret2, OUTPUT);   // sets the pin as output
+} 
+
+void loop() 
+{ 
+  //frem
+  analogWrite(servo, 50);
+  analogWrite(dc_has, 35);
+  digitalWrite(dc_ret1,HIGH);
+  digitalWrite(dc_ret2,LOW);
+  delay(1000);
+  //tilbage
+  analogWrite(servo, 180);
+  analogWrite(dc_has, 35);
+  digitalWrite(dc_ret1,LOW);
+  digitalWrite(dc_ret2,HIGH);
+  delay(1000);
+} 
