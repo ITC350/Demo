c913feea1ec615f2776ad3a0497093e125f0d4ff
Jeppe Nellemann <jepnel@gmail.com>
2017-11-20 11:05:24 +0100

    PWM change

--- motor_test.ino/motor_test.ino.ino
+++ motor_test.ino/motor_test.ino.ino
@@ -1,30 +1,31 @@
-const int servo=3;
+const int servo=5;
 const int dc_has=10;
 const int dc_ret1=11;
 const int dc_ret2=12;
 
 unsigned long tid;
 
-void setup() 
-{ 
+void setup()
+{
+    TCCR3B = TCCR3B & 0b11111000 | 0x04;
   pinMode(servo, OUTPUT);   // sets the pin as output
   pinMode(dc_has, OUTPUT);   // sets the pin as output
   pinMode(dc_ret1, OUTPUT);   // sets the pin as output
   pinMode(dc_ret2, OUTPUT);   // sets the pin as output
-} 
+}
 
-void loop() 
-{ 
+void loop()
+{
   //frem
-  analogWrite(servo, 50);
+  analogWrite(servo, 31);
   analogWrite(dc_has, 35);
   digitalWrite(dc_ret1,HIGH);
   digitalWrite(dc_ret2,LOW);
   delay(1000);
   //tilbage
-  analogWrite(servo, 180);
+  analogWrite(servo, 64);
   analogWrite(dc_has, 35);
   digitalWrite(dc_ret1,LOW);
   digitalWrite(dc_ret2,HIGH);
   delay(1000);
-} 
+}
